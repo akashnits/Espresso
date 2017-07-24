d6793d677944374ad0c1cd086416282c9395ac4d
Akash Raj <Akash.Raj@Lnttechservices.com>
2017-07-24 14:36:01 +0530

    Solution - 4

--- app/src/main/java/com/example/android/teatime/MenuActivity.java
+++ app/src/main/java/com/example/android/teatime/MenuActivity.java
@@ -18,31 +18,42 @@ package com.example.android.teatime;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.example.android.teatime.IdlingResource.SimpleIdlingResource;
 import com.example.android.teatime.model.Tea;
 
 import java.util.ArrayList;
 
 // TODO (1) Implement ImageDownloader.DelayerCallback
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements ImageDownloader.DelayerCallback {
 
     Intent mTeaIntent;
 
     public final static String EXTRA_TEA_NAME = "com.example.android.teatime.EXTRA_TEA_NAME";
 
     // TODO (2) Add a SimpleIdlingResource variable that will be null in production
-
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
     /**
      * TODO (3) Create a method that returns the IdlingResource variable. It will
      * instantiate a new instance of SimpleIdlingResource if the IdlingResource is null.
      * This method will only be called from test.
      */
 
+    public SimpleIdlingResource getIdlingResource(){
+        if(mIdlingResource == null){
+            mIdlingResource= new SimpleIdlingResource();
+            return mIdlingResource;
+        }
+        return mIdlingResource;
+    }
 
     /**
      * TODO (4) Using the method you created, get the IdlingResource variable.
@@ -52,6 +63,7 @@ public class MenuActivity extends AppCompatActivity {
      * onCreate, so there might not be enough time to register the IdlingResource if the download is
      * done too early.
      */
+
 
 
     // TODO (5) Override onDone so when the thread in ImageDownloader is finished, it returns an
@@ -76,6 +88,35 @@ public class MenuActivity extends AppCompatActivity {
 
         // Create a {@link TeaAdapter}, whose data source is a list of {@link Tea}s.
         // The adapter know how to create grid items for each item in the list.
+        GridView gridview = (GridView) findViewById(R.id.tea_grid_view);
+        TeaMenuAdapter adapter = new TeaMenuAdapter(this, R.layout.grid_item_layout, teas);
+        gridview.setAdapter(adapter);
+
+
+        // Set a click listener on that View
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Tea item = (Tea) adapterView.getItemAtPosition(position);
+                // Set the intent to open the {@link OrderActivity}
+                mTeaIntent = new Intent(MenuActivity.this, OrderActivity.class);
+                String teaName = item.getTeaName();
+                mTeaIntent.putExtra(EXTRA_TEA_NAME, teaName);
+                startActivity(mTeaIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mIdlingResource= getIdlingResource();
+        ImageDownloader.downloadImage(this, this, mIdlingResource);
+    }
+
+    @Override
+    public void onDone(ArrayList<Tea> teas) {
         GridView gridview = (GridView) findViewById(R.id.tea_grid_view);
         TeaMenuAdapter adapter = new TeaMenuAdapter(this, R.layout.grid_item_layout, teas);
         gridview.setAdapter(adapter);
